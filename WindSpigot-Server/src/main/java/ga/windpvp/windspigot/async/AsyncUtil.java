1c5864de320b3677f1bb4d42b248eea834552a06
windcolor <teaglehao@gmail.com>
2022-03-16 17:09:06 -0700

    Tweak AsyncUtil.java

--- WindSpigot-Server/src/main/java/ga/windpvp/windspigot/async/AsyncUtil.java
+++ WindSpigot-Server/src/main/java/ga/windpvp/windspigot/async/AsyncUtil.java
@@ -1,11 +1,17 @@
 package ga.windpvp.windspigot.async;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 public class AsyncUtil {
 
+	private static Executor executor = Executors
+			.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat("WindSpigot Async Executor Thread").build());
+
 	public static void run(Runnable runnable) {
-		CompletableFuture.runAsync(runnable);
+		executor.execute(runnable);
 	}
 
 }
