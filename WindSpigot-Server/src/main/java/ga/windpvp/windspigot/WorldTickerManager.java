674c1e837b687e638f8d5976f9d71a3f6dc12052
windcolor <teaglehao@gmail.com>
2022-03-16 18:20:45 -0700

    Disable parallel ticking if there is only one world

--- WindSpigot-Server/src/main/java/ga/windpvp/windspigot/WorldTickerManager.java
+++ WindSpigot-Server/src/main/java/ga/windpvp/windspigot/WorldTickerManager.java
@@ -14,19 +14,19 @@ public class WorldTickerManager {
 
 	// List of cached world tickers
 	private List<WorldTicker> worldTickers = new ArrayList<>();
-	
+
 	// Latch to wait for world tick completion
 	public static volatile CountDownLatch latch = null;
-	
+
 	// Lock for ticking
 	public final static Object lock = new Object();
-	
+
 	// Caches Runnables for less Object creation
 	private void cacheWorlds(boolean isAsync) {
 		if (this.worldTickers.size() != MinecraftServer.getServer().worlds.size()) {
 			worldTickers.clear();
 			for (WorldServer world : MinecraftServer.getServer().worlds) {
-				 worldTickers.add(new WorldTicker(world, isAsync));
+				worldTickers.add(new WorldTicker(world, isAsync));
 			}
 		}
 	}
@@ -34,25 +34,29 @@ public class WorldTickerManager {
 	// Ticks all worlds
 	public void tick() {
 		if (!WindSpigotConfig.parallelWorld) {
-			
+
 			this.cacheWorlds(false);
 
 			for (WorldTicker ticker : this.worldTickers) {
 				ticker.run();
 			}
-		} else {			
+		} else {
 			this.cacheWorlds(true);
-			
-			latch = new CountDownLatch(worldTickers.size());
-			
-			for (WorldTicker ticker : this.worldTickers) {
-				AsyncUtil.run(ticker);
-			}
-			
-			try {
-				latch.await();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+
+			if (this.worldTickers.size() != 1) {
+				latch = new CountDownLatch(worldTickers.size());
+
+				for (WorldTicker ticker : this.worldTickers) {
+					AsyncUtil.run(ticker);
+				}
+
+				try {
+					latch.await();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			} else {
+				this.worldTickers.get(0).run();
 			}
 		}
 	}
