5e1a595fe181fc68c8ecf9393985920eb971d355
windcolor <teaglehao@gmail.com>
2022-03-20 19:20:47 -0700

    More detailed TPS command

--- WindSpigot-Server/src/main/java/org/spigotmc/TicksPerSecondCommand.java
+++ WindSpigot-Server/src/main/java/org/spigotmc/TicksPerSecondCommand.java
@@ -1,8 +1,12 @@
 package org.spigotmc;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.WorldServer;
 
 public class TicksPerSecondCommand extends Command {
 
@@ -26,13 +30,33 @@ public class TicksPerSecondCommand extends Command {
 		for (int i = 0; i < tps.length; i++) {
 			tpsAvg[i] = format(tps[i]);
 		}
+		
+		// WindSpigot - more detailed tps cmd
+		
+		int entityCount = 0;
+		
+		for (WorldServer world : MinecraftServer.getServer().worlds) {
+			entityCount = entityCount + world.entityList.size();
+		}
+		
+		int tileEntityCount = 0;
+		
+		for (WorldServer world : MinecraftServer.getServer().worlds) {
+			tileEntityCount = tileEntityCount + world.tileEntityList.size();
+		}
+		
 		sender.sendMessage(ChatColor.DARK_AQUA + "WindSpigot Performance:");
-		sender.sendMessage(ChatColor.GOLD + "TPS from last 1m, 5m, 15m: "
+		sender.sendMessage(ChatColor.AQUA + "TPS from last 1m, 5m, 15m: "
 				+ org.apache.commons.lang.StringUtils.join(tpsAvg, ", "));
-		sender.sendMessage(ChatColor.GOLD + "Current Memory Usage: " + ChatColor.GREEN
+		sender.sendMessage(ChatColor.AQUA + "Current Memory Usage: " + ChatColor.GREEN
 				+ ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024)) + "/"
 				+ (Runtime.getRuntime().totalMemory() / (1024 * 1024)) + " mb (Max: "
 				+ (Runtime.getRuntime().maxMemory() / (1024 * 1024)) + " mb)");
+		sender.sendMessage(ChatColor.AQUA + "Online Players: " + ChatColor.GREEN + Bukkit.getOnlinePlayers().size());
+		sender.sendMessage(ChatColor.AQUA + "Entity Count: " + ChatColor.GREEN + entityCount);
+		sender.sendMessage(ChatColor.AQUA + "Tile Entity Count: " + ChatColor.GREEN + tileEntityCount);
+		sender.sendMessage(ChatColor.AQUA + "Mob AI: " + ChatColor.GREEN + MinecraftServer.getServer().worlds.get(0).nachoSpigotConfig.enableMobAI);
+
 		return true;
 	}
 
