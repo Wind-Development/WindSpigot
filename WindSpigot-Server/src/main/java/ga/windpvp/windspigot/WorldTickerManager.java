3eecb90013b76a7366940849fef67f123b7d528b
windcolor <teaglehao@gmail.com>
2022-03-23 19:52:05 -0700

    Add comments to world ticker manager

--- WindSpigot-Server/src/main/java/ga/windpvp/windspigot/WorldTickerManager.java
+++ WindSpigot-Server/src/main/java/ga/windpvp/windspigot/WorldTickerManager.java
@@ -31,26 +31,32 @@ public class WorldTickerManager {
 
 	// Caches Runnables for less Object creation
 	private void cacheWorlds(boolean isAsync) {
+		// Only create new world tickers if needed
 		if (this.worldTickers.size() != MinecraftServer.getServer().worlds.size()) {
 			worldTickers.clear();
 						
+			// Create world tickers
 			for (WorldServer world : MinecraftServer.getServer().worlds) {
 				worldTickers.add(new WorldTicker(world, isAsync));
 			}
 			
 			int amountOfWorldTickers = this.worldTickers.size();
 			
+			// Create latch to wait for world ticking to finish
 			if (latch == null) {
 				latch = new ReusableCountLatch(amountOfWorldTickers);
 				return;
 			}
 			
+			// Reuse the latch 
 			if (latch.getCount() > amountOfWorldTickers) {
 				while (latch.getCount() > amountOfWorldTickers) {
+					// Decrease the thread count of the latch if it is too high
 					latch.decrement();
 				}
 			} else if (latch.getCount() < amountOfWorldTickers) {
 				while (latch.getCount() < amountOfWorldTickers) {
+					// Increase the thread count of the latch if it is too low
 					latch.increment();
 				}
 			}
@@ -61,27 +67,34 @@ public class WorldTickerManager {
 	public void tick() {
 		if (!WindSpigotConfig.parallelWorld) {
 
+			// Cache world tick runnables if not cached
 			this.cacheWorlds(false);
 
+			// Tick each world on one thread
 			for (WorldTicker ticker : this.worldTickers) {
 				ticker.run();
 			}
 		} else {
+			// Cache world tick runnables if not cached
 			this.cacheWorlds(true);
 
+			// Only use multiple threads if there are multiple worlds
 			if (this.worldTickers.size() != 1) {
 				//latch = new CountDownLatch(worldTickers.size());
 
+				// Tick each world on a reused thread 
 				for (WorldTicker ticker : this.worldTickers) {
 					AsyncUtil.run(ticker, this.worldTickExecutor);
 				}
 
 				try {
+					// Wait for worlds to finish ticking
 					latch.waitTillZero();
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			} else {
+				// Tick only world on one thread
 				this.worldTickers.get(0).run();
 			}
 		}
