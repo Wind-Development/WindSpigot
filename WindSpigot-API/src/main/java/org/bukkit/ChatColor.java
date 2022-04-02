a03de84bf794f8253f9f4b4a83327de9b202d3cc
128931 <101839340+128931@users.noreply.github.com>
2022-04-02 05:40:50 -0700

    string concatenation in loop

--- WindSpigot-API/src/main/java/org/bukkit/ChatColor.java
+++ WindSpigot-API/src/main/java/org/bukkit/ChatColor.java
@@ -339,7 +339,7 @@ public enum ChatColor {
 	 * @return Any remaining ChatColors to pass onto the next line.
 	 */
 	public static String getLastColors(String input) {
-		String result = "";
+		StringBuilder result = new StringBuilder();
 		int length = input.length();
 
 		// Search backwards from the end as it is faster
@@ -350,7 +350,7 @@ public enum ChatColor {
 				ChatColor color = getByChar(c);
 
 				if (color != null) {
-					result = color + result;
+					result.insert(0, color);
 
 					// Once we find a color or reset we can stop searching
 					if (color.isColor() || color.equals(RESET)) {
@@ -360,7 +360,7 @@ public enum ChatColor {
 			}
 		}
 
-		return result;
+		return result.toString();
 	}
 
 	static {
