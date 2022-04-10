78f79a0254ceca2ec31a919021848226cb15b09d
windcolor <teaglehao@gmail.com>
2022-04-09 17:01:38 -0700

    Fix possible thread safety problem

--- WindSpigot-Server/src/main/java/net/minecraft/server/PlayerList.java
+++ WindSpigot-Server/src/main/java/net/minecraft/server/PlayerList.java
@@ -41,6 +41,7 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.mojang.authlib.GameProfile;
 
+import dev.cobblesword.nachospigot.commons.MCUtils;
 import io.netty.buffer.Unpooled;
 import me.elier.nachospigot.config.NachoConfig;
 
@@ -725,8 +726,12 @@ public abstract class PlayerList {
 		this.b(entityplayer1, worldserver);
 
 		if (!entityplayer.playerConnection.isDisconnected()) {
-			worldserver.getPlayerChunkMap().addPlayer(entityplayer1);
-			worldserver.addEntity(entityplayer1);
+			// WindSpigot start - safe cross world player teleports
+			MCUtils.ensureMain(() -> {
+				worldserver.getPlayerChunkMap().addPlayer(entityplayer1);
+				worldserver.addEntity(entityplayer1);
+			});;
+			// WindSpigot end
 			this.players.add(entityplayer1);
 			this.playersByName.put(entityplayer1.getName(), entityplayer1); // Spigot
 			this.playerMap.put(entityplayer1.getName(), entityplayer1); // PaperSpigot
