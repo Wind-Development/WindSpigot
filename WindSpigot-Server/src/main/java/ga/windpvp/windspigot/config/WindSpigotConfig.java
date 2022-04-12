b98e3c11ba870c7b950b38d6d03fe64184c0f51d
windcolor <teaglehao@gmail.com>
2022-04-11 17:06:24 -0700

    Update config version

--- WindSpigot-Server/src/main/java/ga/windpvp/windspigot/config/WindSpigotConfig.java
+++ WindSpigot-Server/src/main/java/ga/windpvp/windspigot/config/WindSpigotConfig.java
@@ -45,7 +45,7 @@ public class WindSpigotConfig {
 		}
 		config.options().copyDefaults(true);
 
-		int configVersion = 4; // Update this every new configuration update
+		int configVersion = 5; // Update this every new configuration update
 		version = getInt("config-version", configVersion);
 		set("config-version", configVersion);
 		c.setHeader(HEADER);
