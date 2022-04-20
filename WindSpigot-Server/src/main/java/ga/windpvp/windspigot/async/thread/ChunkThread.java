e43e09738b400f13adbfdfc4dee40431bdedb5ae
windcolor-dev <teaglehao@gmail.com>
2022-04-20 09:55:59 -0700

    Finish per player chunk packets

--- WindSpigot-Server/src/main/java/ga/windpvp/windspigot/async/thread/ChunkThread.java
+++ WindSpigot-Server/src/main/java/ga/windpvp/windspigot/async/thread/ChunkThread.java
@@ -3,13 +3,13 @@ package ga.windpvp.windspigot.async.thread;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import ga.windpvp.windspigot.async.netty.Spigot404Write;
 import ga.windpvp.windspigot.config.WindSpigotConfig;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
 import it.unimi.dsi.fastutil.objects.Object2IntArrayMap;
-import javafixes.collection.LinkedArrayQueue;
 import net.minecraft.server.NetworkManager;
 import net.minecraft.server.Packet;
 
@@ -29,13 +29,23 @@ public class ChunkThread extends AsyncPacketThread {
 		// Per player
 		for (NetworkManager manager : playerChunkPackets.keySet()) {
 			
-			//
+			// Register the default packet sends
+			if ((Integer) playerChunkPacketSends.getInt(manager) != null) {
+				playerChunkPacketSends.put(manager, 0);
+			}
+			
+			// Only do this when there are players that need chunk packets
 			while (playerChunkPackets.get(manager).size() != 0) {
+				
+				// Stop sending after 5 packets
+				if (playerChunkPacketSends.getInt(manager) == WindSpigotConfig.maxChunkSends) {
+					playerChunkPacketSends.put(manager, 0);
+					return;
+				}
+				// Send the chunk packet
                 Spigot404Write.writeThenFlush(manager.channel, playerChunkPackets.get(manager).poll(), null);
                 
-                if ((Integer) playerChunkPacketSends.getInt(manager) != null) {
-                	 playerChunkPacketSends.put(manager, 0);
-                }
+                // Register the chunk send count
                 playerChunkPacketSends.put(manager, playerChunkPacketSends.getInt(manager) + 1);
 			}
 			
@@ -43,13 +53,19 @@ public class ChunkThread extends AsyncPacketThread {
 	}
 	
     // Queue a packet
+	@Override
     public void addPacket(final Packet<?>  packet, final NetworkManager manager, final GenericFutureListener<? extends Future<? super Void>>[] agenericfuturelistener) {
     	if (this.playerChunkPackets.get(manager) != null) {
     		this.playerChunkPackets.get(manager).add(packet);
     	} else {
-    		Queue<Packet<?>> queue = new LinkedArrayQueue<Packet<?>>();
+    		Queue<Packet<?>> queue = new ConcurrentLinkedQueue<Packet<?>>();
     		queue.add(packet);
         	this.playerChunkPackets.put(manager, queue);
     	}
     }
+	
+	public void removePlayer(NetworkManager manager) {
+		this.playerChunkPackets.remove(manager);
+		this.playerChunkPacketSends.remove(manager);
+	}
 }
