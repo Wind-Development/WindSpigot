c907040c313e879e7c7c0ca46035b291bbd78f9c
windcolor-dev <teaglehao@gmail.com>
2022-04-20 15:11:19 -0700

    Adaptive chunk packet sending + fix thread safety problems

--- WindSpigot-Server/src/main/java/ga/windpvp/windspigot/async/thread/ChunkThread.java
+++ WindSpigot-Server/src/main/java/ga/windpvp/windspigot/async/thread/ChunkThread.java
@@ -1,9 +1,11 @@
 package ga.windpvp.windspigot.async.thread;
 
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+
+import com.google.common.collect.Maps;
 
 import ga.windpvp.windspigot.async.netty.Spigot404Write;
 import ga.windpvp.windspigot.config.WindSpigotConfig;
@@ -15,7 +17,8 @@ import net.minecraft.server.Packet;
 
 public class ChunkThread extends AsyncPacketThread {
 
-	private Map<NetworkManager, Queue<Packet<?>>> playerChunkPackets = new ConcurrentHashMap<>();
+	// These do not need to be concurrent collections due to synchronization
+	private Map<NetworkManager, Queue<Packet<?>>> playerChunkPackets = Maps.newHashMap();
 	private Object2IntArrayMap<NetworkManager> playerChunkPacketSends = new Object2IntArrayMap<>();
 	
 	public ChunkThread(String s) {
@@ -26,46 +29,71 @@ public class ChunkThread extends AsyncPacketThread {
 	// Handle chunk packets
 	@Override
 	public void run() {
-		// Per player
-		for (NetworkManager manager : playerChunkPackets.keySet()) {
-			
-			// Register the default packet sends
-			if ((Integer) playerChunkPacketSends.getInt(manager) != null) {
-				playerChunkPacketSends.put(manager, 0);
-			}
-			
-			// Only do this when there are players that need chunk packets
-			while (playerChunkPackets.get(manager).size() != 0) {
-				
-				// Stop sending after 5 packets
-				if (playerChunkPacketSends.getInt(manager) == WindSpigotConfig.maxChunkSends) {
-					playerChunkPacketSends.put(manager, 0);
-					return;
+		// Synchronize
+		synchronized (playerChunkPackets) {
+			synchronized (playerChunkPacketSends) {
+
+				// Per player
+				for (NetworkManager manager : playerChunkPackets.keySet()) {
+
+					// Register the default packet sends
+					if ((Integer) playerChunkPacketSends.getInt(manager) != null) {
+						playerChunkPacketSends.put(manager, 0);
+					}
+
+					// Only do this when there are players that need chunk packets
+					while (playerChunkPackets.get(manager).size() != 0) {
+
+						// Stop sending after 5 packets
+						if (playerChunkPacketSends.getInt(manager) == WindSpigotConfig.maxChunkSends) {
+							playerChunkPacketSends.put(manager, 0);
+							return;
+						}
+						
+						// Send the chunk packet
+						Spigot404Write.writeThenFlush(manager.channel, playerChunkPackets.get(manager).poll(), null);
+
+						// Register the chunk send count
+						playerChunkPacketSends.put(manager, playerChunkPacketSends.get(manager) + 1);
+					}
 				}
-				// Send the chunk packet
-                Spigot404Write.writeThenFlush(manager.channel, playerChunkPackets.get(manager).poll(), null);
-                
-                // Register the chunk send count
-                playerChunkPacketSends.put(manager, playerChunkPacketSends.getInt(manager) + 1);
+				
 			}
-			
 		}
 	}
 	
-    // Queue a packet
+	// Queue a packet
 	@Override
-    public void addPacket(final Packet<?>  packet, final NetworkManager manager, final GenericFutureListener<? extends Future<? super Void>>[] agenericfuturelistener) {
-    	if (this.playerChunkPackets.get(manager) != null) {
-    		this.playerChunkPackets.get(manager).add(packet);
-    	} else {
-    		Queue<Packet<?>> queue = new ConcurrentLinkedQueue<Packet<?>>();
-    		queue.add(packet);
-        	this.playerChunkPackets.put(manager, queue);
-    	}
-    }
+	public void addPacket(final Packet<?> packet, final NetworkManager manager,
+			final GenericFutureListener<? extends Future<? super Void>>[] agenericfuturelistener) {
+		// Synchronize
+		synchronized (this.playerChunkPackets) {
+			synchronized (this.playerChunkPacketSends) {
+				
+				// Add packet to queue if it already exists
+				if (this.playerChunkPackets.get(manager) != null) {
+					this.playerChunkPackets.get(manager).add(packet);
+				} else {
+					// Create a queue, put the packet, and map the queue
+					Queue<Packet<?>> queue = new LinkedList<Packet<?>>(); // No need for thread safe collection because we synchronize
+					queue.add(packet);
+					this.playerChunkPackets.put(manager, queue);
+				}
+				
+			}
+		}
+	}
 	
 	public void removePlayer(NetworkManager manager) {
-		this.playerChunkPackets.remove(manager);
-		this.playerChunkPacketSends.remove(manager);
+		// Synchronize
+		synchronized (this.playerChunkPackets) {
+			synchronized (this.playerChunkPacketSends) {
+				
+				// Clear caches of packets and player packet sends
+				this.playerChunkPackets.remove(manager);
+				this.playerChunkPacketSends.remove(manager);
+			}
+			
+		}
 	}
 }
