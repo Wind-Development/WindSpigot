e1f6c5581d76987ae5e7591ea1d739b3641852ed
windcolor-dev <teaglehao@gmail.com>
2022-04-25 10:37:46 -0700

    Possibly fix null pointer

--- WindSpigot-Server/src/main/java/ga/windpvp/windspigot/gui/BaseGui.java
+++ WindSpigot-Server/src/main/java/ga/windpvp/windspigot/gui/BaseGui.java
@@ -3,13 +3,17 @@ package ga.windpvp.windspigot.gui;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.Inventory;
 
+import dev.cobblesword.nachospigot.commons.MCUtils;
+
 public abstract class BaseGui {
 
 	protected Inventory inventory;
 
 	public BaseGui(String name) {
-		inventory = Bukkit.createInventory(null, 36, name);
-		initItems();
+		MCUtils.ensureMain(() -> {
+			inventory = Bukkit.createInventory(null, 36, name);
+			initItems();
+		}); // Run next tick, server might not be initialized
 	}
 
 	protected void initItems() {
