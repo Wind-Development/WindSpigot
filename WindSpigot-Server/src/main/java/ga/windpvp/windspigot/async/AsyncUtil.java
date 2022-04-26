09fe85d76f2f26692a0eb311d588c739dde1a313
windcolor-dev <teaglehao@gmail.com>
2022-04-26 16:28:10 -0700

    [skip] Add comments

--- WindSpigot-Server/src/main/java/ga/windpvp/windspigot/async/AsyncUtil.java
+++ WindSpigot-Server/src/main/java/ga/windpvp/windspigot/async/AsyncUtil.java
@@ -5,10 +5,19 @@ import java.util.concurrent.ForkJoinPool;
 
 public class AsyncUtil {
 
+	/**
+	 * Runs a given task async
+	 * @param runnable The task to run
+	 */
 	public static void run(Runnable runnable) {
 		ForkJoinPool.commonPool().execute(runnable);
 	}
 	
+	/**
+	 * Runs a given task on a specified Executor
+	 * @param runnable The task to run
+	 * @param executor The executor to run this task on
+	 */
 	public static void run(Runnable runnable, Executor executor) {
 		executor.execute(runnable);
 	}
