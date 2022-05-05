1cfe0abe1657862c079d85064fbf917a13c29abf
windcolor-dev <teaglehao@gmail.com>
2022-05-05 10:50:15 -0700

    Prevent overwrites of preset kb profile changes

--- WindSpigot-Server/src/main/java/dev/cobblesword/nachospigot/knockback/KnockbackConfig.java
+++ WindSpigot-Server/src/main/java/dev/cobblesword/nachospigot/knockback/KnockbackConfig.java
@@ -43,20 +43,26 @@ public class KnockbackConfig {
 
 		save();
 
-		final KnockbackProfile kohiProfile = new CraftKnockbackProfile("kohi");
-		kohiProfile.save(true);
+		Set<String> keys = getKeys("knockback.profiles");
 		
-		final KnockbackProfile defaultProfile = new CraftKnockbackProfile("vanilla"); 
-		// Generated profiles are based on kohi kb values, not vanilla, so we set the values here
-		defaultProfile.setFrictionHorizontal(2.0);
-		defaultProfile.setFrictionVertical(2.0);
-		defaultProfile.setHorizontal(0.35);
-		defaultProfile.setVertical(0.35);
-		defaultProfile.setExtraHorizontal(0.425);
-		defaultProfile.setExtraVertical(0.085);
-		defaultProfile.save(true);
-
-		for (String key : getKeys("knockback.profiles")) {
+		if (!keys.contains("kohi")) {
+			final KnockbackProfile kohiProfile = new CraftKnockbackProfile("kohi");
+			kohiProfile.save(true);
+		}
+		
+		if (!keys.contains("vanilla")) {
+			final KnockbackProfile defaultProfile = new CraftKnockbackProfile("vanilla"); 
+			// Generated profiles are based on kohi kb values, not vanilla, so we set the values here
+			defaultProfile.setFrictionHorizontal(2.0);
+			defaultProfile.setFrictionVertical(2.0);
+			defaultProfile.setHorizontal(0.35);
+			defaultProfile.setVertical(0.35);
+			defaultProfile.setExtraHorizontal(0.425);
+			defaultProfile.setExtraVertical(0.085);
+			defaultProfile.save(true);
+		}
+		
+		for (String key : keys) {
 			final String path = "knockback.profiles." + key;
 			CraftKnockbackProfile profile = (CraftKnockbackProfile) getKbProfileByName(key);
 			if (profile == null) {
@@ -86,8 +92,11 @@ public class KnockbackConfig {
 		}
 		currentKb = getKbProfileByName(getString("knockback.current", "kohi"));
 		if (currentKb == null) {
-			WindSpigot.LOGGER.warn("Knockback profile 'default' was not found, using profile 'kohi' for now!");
-			currentKb = kohiProfile;
+			WindSpigot.LOGGER.warn("Knockback profile selected was not found, using profile 'kohi' for now!");
+			currentKb = getKbProfileByName("kohi");
+			
+			WindSpigot.LOGGER.info("Setting default knockback as 'kohi'...");
+			set("knockback.current", "kohi");
 		}
 	}
 
