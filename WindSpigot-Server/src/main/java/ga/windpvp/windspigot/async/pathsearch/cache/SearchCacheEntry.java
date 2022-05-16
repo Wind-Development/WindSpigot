9907947f2a3ffff7ce00cac67554d08210bf8bd7
windcolor-dev <teaglehao@gmail.com>
2022-05-15 20:59:52 -0700

    Respect pathfind accuracy setting

--- WindSpigot-Server/src/main/java/ga/windpvp/windspigot/async/pathsearch/cache/SearchCacheEntry.java
+++ WindSpigot-Server/src/main/java/ga/windpvp/windspigot/async/pathsearch/cache/SearchCacheEntry.java
@@ -1,6 +1,7 @@
 package ga.windpvp.windspigot.async.pathsearch.cache;
 
 import net.minecraft.server.EntityInsentient;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PathEntity;
 
 // A cache entry for general targeting to use after an async path search
@@ -8,10 +9,12 @@ public class SearchCacheEntry {
 
 	private final EntityInsentient targetingEntity;
 	private final PathEntity path;
+	private final int tick;
 
 	public SearchCacheEntry(EntityInsentient targetingEntity, PathEntity path) {
 		this.targetingEntity = targetingEntity;
 		this.path = path;
+		this.tick = MinecraftServer.currentTick;
 	}
 
 	public EntityInsentient getTargetingEntity() {
@@ -20,6 +23,14 @@ public class SearchCacheEntry {
 	
 	public PathEntity getPath() {
 		return path;
+	}
+
+	public int getTick() {
+		return tick;
+	}
+	
+	public boolean isAccurate() {
+		return MinecraftServer.currentTick - tick < 3;
 	}
 
 }
