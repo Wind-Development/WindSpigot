e41b570d54fe5eb8cb7eaa6b699b785a15984b1a
windcolor-dev <teaglehao@gmail.com>
2022-05-22 19:40:18 -0700

    [skip] Add some missing comments

--- WindSpigot-Server/src/main/java/net/minecraft/server/EntityTrackerEntry.java
+++ WindSpigot-Server/src/main/java/net/minecraft/server/EntityTrackerEntry.java
@@ -140,7 +140,8 @@ public class EntityTrackerEntry {
 		this.xRot = MathHelper.d(entity.pitch * 256.0F / 360.0F);
 		this.lastHeadYaw = MathHelper.d(entity.getHeadRotation() * 256.0F / 360.0F);
 		this.lastOnGround = entity.onGround;
-
+		
+		// WindSpigot - async entity tracking
 		if (WindSpigotConfig.disableTracking) {
 			this.addRemoveRate = 100;
 		} else if (this.tracker instanceof EntityArrow || this.tracker instanceof EntityProjectile) {
@@ -165,7 +166,8 @@ public class EntityTrackerEntry {
 	public int hashCode() {
 		return this.tracker.getId();
 	}
-
+	
+	// WindSpigot start - async entity tracking
 	// WindSpigot - synchronize
 	public synchronized void update() {
 		this.withinNoTrack = this.withinNoTrack();
@@ -186,12 +188,15 @@ public class EntityTrackerEntry {
 		}
 
 		for (EntityPlayer entityplayer : (Collection<EntityPlayer>) trackedPlayers) {
-			double d0 = entityplayer.locX - this.tracker.locX;
-			double d1 = entityplayer.locZ - this.tracker.locZ;
-			int range = this.getRange();
-
-			if (!(d0 >= (-range) && d0 <= range && d1 >= (-range) && d1 <= range) || withinNoTrack()) {
-				toRemove.add(entityplayer);
+			// WindSpigot - synchronize
+			synchronized (entityplayer) {
+				double d0 = entityplayer.locX - this.tracker.locX;
+				double d1 = entityplayer.locZ - this.tracker.locZ;
+				int range = this.getRange();
+	
+				if (!(d0 >= (-range) && d0 <= range && d1 >= (-range) && d1 <= range) || withinNoTrack()) {
+					toRemove.add(entityplayer);
+				}
 			}
 		}
 
@@ -246,6 +251,7 @@ public class EntityTrackerEntry {
 			}
 		}
 	};
+	// WindSpigot end
 
 	/**
 	 * sends velocity, Location, rotation, and riding info.
