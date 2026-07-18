210476dd56fceaaf86f5652e50e8aa5e533fb564
Elias <elias@falchus.com>
2026-07-18 03:06:04 +0200

    Update DedicatedServer.java

--- WindSpigot-Server/src/main/java/net/minecraft/server/DedicatedServer.java
+++ WindSpigot-Server/src/main/java/net/minecraft/server/DedicatedServer.java
@@ -9,7 +9,6 @@ import java.net.Proxy;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
-import ga.windpvp.windspigot.random.FastRandom;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -292,7 +291,7 @@ public class DedicatedServer extends MinecraftServer implements IMinecraftServer
 				String s = this.propertyManager.getString("level-seed", "");
 				String s1 = this.propertyManager.getString("level-type", "DEFAULT");
 				String s2 = this.propertyManager.getString("generator-settings", "");
-				long k = (new FastRandom()).nextLong();
+				long k = (new Random()).nextLong();
 
 				if (s.length() > 0) {
 					try {
