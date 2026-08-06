11388aedd8a1fb5e67705f94ba436c2743605cd5
Elias <elias@falchus.com>
2026-08-06 16:38:11 +0200

    Update ChunkRegionLoader.java

--- WindSpigot-Server/src/main/java/net/minecraft/server/ChunkRegionLoader.java
+++ WindSpigot-Server/src/main/java/net/minecraft/server/ChunkRegionLoader.java
@@ -45,9 +45,7 @@ public class ChunkRegionLoader implements IChunkLoader, IAsyncChunkSaver {
 	// CraftBukkit start - Add async variant, provide compatibility
 	@Override
 	public Chunk a(World world, int i, int j) throws IOException {
-		world.timings.syncChunkLoadDataTimer.startTiming(); // Spigot
 		Object[] data = loadChunk(world, i, j);
-		world.timings.syncChunkLoadDataTimer.stopTiming(); // Spigot
 		if (data != null) {
 			Chunk chunk = (Chunk) data[0];
 			NBTTagCompound nbttagcompound = (NBTTagCompound) data[1];
@@ -413,7 +411,6 @@ public class ChunkRegionLoader implements IChunkLoader, IAsyncChunkSaver {
 
 	public void loadEntities(Chunk chunk, NBTTagCompound nbttagcompound, World world) {
 		// CraftBukkit end
-		world.timings.syncChunkLoadEntitiesTimer.startTiming(); // Spigot
 		NBTTagList nbttaglist1 = nbttagcompound.getList("Entities", 10);
 
 		if (nbttaglist1 != null) {
@@ -440,8 +437,6 @@ public class ChunkRegionLoader implements IChunkLoader, IAsyncChunkSaver {
 				}
 			}
 		}
-		world.timings.syncChunkLoadEntitiesTimer.stopTiming(); // Spigot
-		world.timings.syncChunkLoadTileEntitiesTimer.startTiming(); // Spigot
 		NBTTagList nbttaglist2 = nbttagcompound.getList("TileEntities", 10);
 
 		if (nbttaglist2 != null) {
@@ -454,8 +449,6 @@ public class ChunkRegionLoader implements IChunkLoader, IAsyncChunkSaver {
 				}
 			}
 		}
-		world.timings.syncChunkLoadTileEntitiesTimer.stopTiming(); // Spigot
-		world.timings.syncChunkLoadTileTicksTimer.startTiming(); // Spigot
 
 		if (nbttagcompound.hasKeyOfType("TileTicks", 9)) {
 			NBTTagList nbttaglist3 = nbttagcompound.getList("TileTicks", 10);
@@ -477,7 +470,6 @@ public class ChunkRegionLoader implements IChunkLoader, IAsyncChunkSaver {
 				}
 			}
 		}
-		world.timings.syncChunkLoadTileTicksTimer.stopTiming(); // Spigot
 
 		// return chunk; // CraftBukkit
 	}
