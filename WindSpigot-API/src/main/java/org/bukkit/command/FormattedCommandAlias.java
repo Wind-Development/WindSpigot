bbdbfa308ed44f2591f68aae5e5485d1cfe7e16b
Elias <elias@falchus.com>
2026-08-06 16:50:04 +0200

    Update FormattedCommandAlias.java

--- WindSpigot-API/src/main/java/org/bukkit/command/FormattedCommandAlias.java
+++ WindSpigot-API/src/main/java/org/bukkit/command/FormattedCommandAlias.java
@@ -9,7 +9,6 @@ public class FormattedCommandAlias extends Command {
 
 	public FormattedCommandAlias(String alias, String[] formatStrings) {
 		super(alias);
-		timings = co.aikar.timings.TimingsManager.getCommandTiming("minecraft", this); // Spigot
 		this.formatStrings = formatStrings;
 	}
 
@@ -114,11 +113,6 @@ public class FormattedCommandAlias extends Command {
 
 		return formatString;
 	}
-
-	@Override // Spigot
-	public String getTimingName() {
-		return "Command Forwarder - " + super.getTimingName();
-	} // Spigot
 
 	private static boolean inRange(int i, int j, int k) {
 		return i >= j && i <= k;
