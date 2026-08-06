12e30d702b902928852c5a30e5a8e307563a253d
Elias <elias@falchus.com>
2026-08-06 16:44:42 +0200

    Update WorldServer.java

--- WindSpigot-Server/src/main/java/net/minecraft/server/WorldServer.java
+++ WindSpigot-Server/src/main/java/net/minecraft/server/WorldServer.java
@@ -254,18 +254,15 @@ public class WorldServer extends World implements IAsyncTaskHandler {
 		if (this.getGameRules().getBoolean("doMobSpawning")
 				&& this.worldData.getType() != WorldType.DEBUG_ALL_BLOCK_STATES
 				&& (this.allowMonsters || this.allowAnimals) && this.players.size() > 0) {
-			timings.mobSpawn.startTiming(); // Spigot
 			this.R.a(this,
 					this.allowMonsters
 							&& (this.ticksPerMonsterSpawns != 0 && time % this.ticksPerMonsterSpawns == 0L),
 					this.allowAnimals && (this.ticksPerAnimalSpawns != 0 && time % this.ticksPerAnimalSpawns == 0L),
 					this.worldData.getTime() % 400L == 0L);
-			timings.mobSpawn.stopTiming(); // Spigot
 			// CraftBukkit end
 		}
 		// CraftBukkit end
 		if (this.nachoSpigotConfig.doChunkUnload) {
-			timings.doChunkUnload.startTiming(); // Spigot
 			this.methodProfiler.c("chunkSource");
 
 			// Only unload if chunkProvider isn't null
@@ -285,35 +282,22 @@ public class WorldServer extends World implements IAsyncTaskHandler {
 			this.worldData.setDayTime(this.worldData.getDayTime() + 1L);
 		}
 
-		timings.doChunkUnload.stopTiming(); // Spigot
 		this.methodProfiler.c("tickPending");
-		timings.scheduledBlocks.startTiming(); // Spigot
 		this.a(false);
-		timings.scheduledBlocks.stopTiming(); // Spigot
 		this.methodProfiler.c("tickBlocks");
-		timings.chunkTicks.startTiming(); // Spigot
 		this.h();
-		timings.chunkTicks.stopTiming(); // Spigot
 		spigotConfig.antiXrayInstance.flushUpdates(this); // PaperSpigot
 		this.methodProfiler.c("chunkMap");
-		timings.doChunkMap.startTiming(); // Spigot
 		this.manager.flush();
-		timings.doChunkMap.stopTiming(); // Spigot
 		this.methodProfiler.c("village");
-		timings.doVillages.startTiming(); // Spigot
 		this.villages.tick();
 		this.siegeManager.a();
-		timings.doVillages.stopTiming(); // Spigot
 		this.methodProfiler.c("portalForcer");
-		timings.doPortalForcer.startTiming(); // Spigot
 		this.Q.a(this.getTime());
-		timings.doPortalForcer.stopTiming(); // Spigot
 		this.methodProfiler.b();
-		timings.doSounds.startTiming(); // Spigot
 		this.ak();
 
 		this.getWorld().processChunkGC(); // CraftBukkit
-		timings.doChunkGC.stopTiming(); // Spigot
 	}
 
 	public BiomeBase.BiomeMeta a(EnumCreatureType enumcreaturetype, BlockPosition blockposition) {
@@ -539,7 +523,6 @@ public class WorldServer extends World implements IAsyncTaskHandler {
 
 				if (this.nachoSpigotConfig.doBlocksOperations) {
 					this.methodProfiler.c("tickBlocks");
-					timings.chunkTicksBlocks.startTiming(); // Spigot
 					int randomTickSpeed = this.getGameRules().c("randomTickSpeed");
 					if (randomTickSpeed > 0) {
 						for (ChunkSection section : chunk.getSections()) {
@@ -565,7 +548,6 @@ public class WorldServer extends World implements IAsyncTaskHandler {
 						}
 					}
 				}
-				timings.chunkTicksBlocks.stopTiming(); // Spigot
 			}
 
 		}
@@ -702,7 +684,6 @@ public class WorldServer extends World implements IAsyncTaskHandler {
 
 			this.methodProfiler.a("cleaning");
 
-			timings.scheduledBlocksCleanup.startTiming(); // Spigot
 			NextTickListEntry nextticklistentry;
 
 			for (int j = 0; j < i; ++j) {
@@ -715,7 +696,6 @@ public class WorldServer extends World implements IAsyncTaskHandler {
 				this.M.remove(nextticklistentry);
 				this.V.add(nextticklistentry);
 			}
-			timings.scheduledBlocksCleanup.stopTiming(); // Spigot
 
 			// PaperSpigot start - Allow redstone ticks to bypass the tickNextTickListCap
 			/*
@@ -731,7 +711,6 @@ public class WorldServer extends World implements IAsyncTaskHandler {
 
 			this.methodProfiler.b();
 			this.methodProfiler.a("ticking");
-			timings.scheduledBlocksTicking.startTiming(); // Spigot
 			Iterator iterator = this.V.iterator();
 
 			while (iterator.hasNext()) {
@@ -742,8 +721,6 @@ public class WorldServer extends World implements IAsyncTaskHandler {
 				if (this.areChunksLoadedBetween(nextticklistentry.a.a(-b0, -b0, -b0),
 						nextticklistentry.a.a(b0, b0, b0))) {
 					IBlockData iblockdata = this.getType(nextticklistentry.a);
-					co.aikar.timings.Timing timing = iblockdata.getBlock().getTiming(); // Spigot
-					timing.startTiming(); // Spigot
 
 					if (iblockdata.getBlock().getMaterial() != Material.AIR
 							&& Block.a(iblockdata.getBlock(), nextticklistentry.a())) {
@@ -757,12 +734,10 @@ public class WorldServer extends World implements IAsyncTaskHandler {
 							throw new ReportedException(crashreport);
 						}
 					}
-					timing.stopTiming(); // Spigot
 				} else {
 					this.a(nextticklistentry.a, nextticklistentry.a(), 0);
 				}
 			}
-			timings.scheduledBlocksTicking.stopTiming(); // Spigot
 
 			this.methodProfiler.b();
 			this.V.clear();
