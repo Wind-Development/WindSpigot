a7f74f1ed3925b4cf6a2d88a46aea6f9ef9e1959
Elias <elias@falchus.com>
2026-08-18 00:23:33 +0200

    fix: remove broken world ticking cache (#157)
    * Update WorldTickManager.java
    
    * Update WorldTickManager.java
    
    * Update WorldTickManager.java
    
    * Update WorldTickManager.java
    
    * Update WindSpigot-Server/src/main/java/com/windpvp/windspigot/world/WorldTickManager.java
    
    Co-authored-by: windcolor-dev <79166388+windcolor-dev@users.noreply.github.com>
    
    * Update WorldTickManager.java
    
    ---------
    
    Co-authored-by: windcolor-dev <79166388+windcolor-dev@users.noreply.github.com>

--- WindSpigot-Server/src/main/java/com/windpvp/windspigot/world/WorldTickManager.java
+++ WindSpigot-Server/src/main/java/com/windpvp/windspigot/world/WorldTickManager.java
@@ -1,18 +1,10 @@
 package com.windpvp.windspigot.world;
 
-import java.util.List;
-import com.google.common.collect.Lists;
-import com.windpvp.windspigot.config.WindSpigotConfig;
-
 import co.aikar.timings.SpigotTimings;
-import net.minecraft.server.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.WorldServer;
 
 public class WorldTickManager {
-
-	// List of cached world tickers
-	private final List<WorldTicker> worldTickers = Lists.newArrayList();
 
 	// Instance
 	private static WorldTickManager worldTickerManagerInstance;
@@ -23,39 +15,25 @@ public class WorldTickManager {
 
 	}
 
-	private void cacheWorlds() {
-		// Only create new world tickers if needed
-		if (this.worldTickers.size() != MinecraftServer.getServer().worlds.size()) {
-			worldTickers.clear();
-						
-			// Create world tickers
-			for (WorldServer world : MinecraftServer.getServer().worlds) {
-				WorldTicker ticker = new WorldTicker(world);
-				worldTickers.add(ticker);	
-				world.ticker = ticker;
-			}
-
-		}
-	}
-
 	// Ticks all worlds
 	public void tick() {
 		tickWorlds();
 	}
 	
 	private void tickWorlds() {
-        // Cache world tick runnables if not cached already
-        this.cacheWorlds();
-
         // Move BukkitScheduler stuff here so async entity tracking does not interfere
         SpigotTimings.bukkitSchedulerTimer.startTiming(); // Spigot
         // CraftBukkit start
         MinecraftServer.getServer().server.getScheduler().mainThreadHeartbeat(MinecraftServer.getServer().at());
         SpigotTimings.bukkitSchedulerTimer.stopTiming(); // Spigot
-        
-        for (WorldTicker ticker : this.worldTickers) {
-            ticker.run();
-        }
+	        
+		for (int i = 0; i < MinecraftServer.getServer().worlds.size(); i++) {
+			WorldServer world = MinecraftServer.getServer().worlds.get(i);
+			if (world.ticker == null) {
+				world.ticker = new WorldTicker(world);
+			}
+			world.ticker.run();
+		}
 	}
 
 	/*
