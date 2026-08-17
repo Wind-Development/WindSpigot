828780792a80ecccc088a7cac8985c7047c4f9c3
Elias <elias@falchus.com>
2026-08-18 00:14:06 +0200

    Update WorldTickManager.java

--- WindSpigot-Server/src/main/java/com/windpvp/windspigot/world/WorldTickManager.java
+++ WindSpigot-Server/src/main/java/com/windpvp/windspigot/world/WorldTickManager.java
@@ -26,14 +26,14 @@ public class WorldTickManager {
         // CraftBukkit start
         MinecraftServer.getServer().server.getScheduler().mainThreadHeartbeat(MinecraftServer.getServer().at());
         SpigotTimings.bukkitSchedulerTimer.stopTiming(); // Spigot
-        
-	for (int i = 0; i < MinecraftServer.getServer().worlds.size(); ++i) {
-            WorldServer world = MinecraftServer.getServer().worlds.get(i);
+	        
+		for (int i = 0; i < MinecraftServer.getServer().worlds.size(); i++) {
+			WorldServer world = MinecraftServer.getServer().worlds.get(i);
 			if (world.ticker == null) {
-            	world.ticker = new WorldTicker(world);
+				world.ticker = new WorldTicker(world);
 			}
 			world.ticker.run();
-        }
+		}
 	}
 
 	/*
