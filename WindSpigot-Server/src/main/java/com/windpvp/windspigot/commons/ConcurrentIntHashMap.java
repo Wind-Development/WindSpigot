986ce215f4dc3b62a553d6df90caef31a776130d
GamingOP69 <mrbeastshorts18@gmail.com>
2026-08-17 22:32:21 +0545

    perf: eliminate lambda allocations in ConcurrentIntHashMap

--- WindSpigot-Server/src/main/java/com/windpvp/windspigot/commons/ConcurrentIntHashMap.java
+++ WindSpigot-Server/src/main/java/com/windpvp/windspigot/commons/ConcurrentIntHashMap.java
@@ -1,28 +1,31 @@
 package com.windpvp.windspigot.commons;
 
-import static com.windpvp.windspigot.async.AsyncUtil.runSynchronized;
-
 import net.minecraft.server.IntHashMap;
 
 public class ConcurrentIntHashMap<V> extends IntHashMap<V> {
 	
-	public V get(int var1) {
-		return runSynchronized(this, () -> super.get(var1));
+	@Override
+	public synchronized V get(int var1) {
+		return super.get(var1);
 	}
 
-	public boolean b(int var1) {
-		return runSynchronized(this, () -> super.b(var1));
+	@Override
+	public synchronized boolean b(int var1) {
+		return super.b(var1);
 	}
 
-	public void a(int var1, V var2) {
-		runSynchronized(this, () -> super.a(var1, var2));
+	@Override
+	public synchronized void a(int var1, V var2) {
+		super.a(var1, var2);
 	}
 
-	public V d(int var1) {
-		return runSynchronized(this, () -> super.d(var1));
+	@Override
+	public synchronized V d(int var1) {
+		return super.d(var1);
 	}
 
-	public void c() {
-		runSynchronized(this, super::c);
+	@Override
+	public synchronized void c() {
+		super.c();
 	}
 }
