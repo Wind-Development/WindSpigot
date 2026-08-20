26bb86b39dd3a0086b2149bbc3dc78ea52b7986c
GamingOP69 <mrbeastshorts18@gmail.com>
2026-08-20 09:28:38 +0545

    Refactor comments for null safety implementation

--- WindSpigot-Server/src/main/java/net/minecraft/server/HandshakeListener.java
+++ WindSpigot-Server/src/main/java/net/minecraft/server/HandshakeListener.java
@@ -90,7 +90,7 @@ public class HandshakeListener implements PacketHandshakingInListener {
 							return;
 						}
 
-						// WindSpigot start - GamingOP69 - null safety for TCPShield and PlayerHandshakeEvent
+						// WindSpigot start - null safety for TCPShield and PlayerHandshakeEvent
 						if (event.getServerHostname() != null) {
 							packethandshakinginsetprotocol.hostname = event.getServerHostname();
 						}
@@ -105,7 +105,7 @@ public class HandshakeListener implements PacketHandshakingInListener {
 							this.b.spoofedProfile = gson.fromJson(event.getPropertiesJson(),
 									com.mojang.authlib.properties.Property[].class);
 						}
-						// WindSpigot end - GamingOP69
+						// WindSpigot end
 						handledByEvent = true; // Hooray, we did it!
 					}
 				}
