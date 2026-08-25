d504f3aa1f0d5e6a7d3a62cf072e570ac03f84eb
Elias <elias@falchus.com>
2026-08-25 02:23:10 +0200

    Update PlayerConnectionUtils.java

--- WindSpigot-Server/src/main/java/net/minecraft/server/PlayerConnectionUtils.java
+++ WindSpigot-Server/src/main/java/net/minecraft/server/PlayerConnectionUtils.java
@@ -1,9 +1,14 @@
 package net.minecraft.server;
 
 public class PlayerConnectionUtils {
-	public static <T extends PacketListener> void ensureMainThread(final Packet<T> packet, final T listener,
-			IAsyncTaskHandler handler) throws CancelledPacketHandleException {
-		if (!packet.instant() && !handler.isMainThread()) {
+    // PandaSpigot start - Redirect to MinecraftServer so that IAsyncTaskHandler can handle it
+	public static <T extends PacketListener> void ensureMainThread(final Packet<T> packet, final T listener, WorldServer worldServer) throws CancelledPacketHandleException {
+        ensureMainThread(packet, listener, worldServer.getMinecraftServer());
+    }
+
+    public static <T extends PacketListener> void ensureMainThread(final Packet<T> packet, final T listener, IAsyncTaskHandler<?> handler) throws CancelledPacketHandleException {
+    // PandaSpigot end
+		if (!handler.isMainThread()) {
 			handler.postToMainThread(() -> packet.a(listener));
 			throw CancelledPacketHandleException.INSTANCE;
 		}
